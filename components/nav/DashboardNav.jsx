be3d3718fe008be55236db79d1a8b1f31125a118
William <anazawilliam1@gmail.com>
2024-03-20 03:44:02 +0100

    mobile menu

--- components/nav/DashboardNav.jsx
+++ components/nav/DashboardNav.jsx
@@ -1,13 +1,20 @@
+"use client";
+
 import Link from "next/link";
 import CTA from "../button/CTA";
 import {
+  faBarsStaggered,
   faBell,
   faBoltLightning,
   faGear,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 const DashboardNav = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <header>
       <nav className="fixed w-full shadow bg-white/60 backdrop-blur-md z-20">
@@ -57,6 +64,49 @@ const DashboardNav = () => {
               <div className="hidden md:flex">
                 <CTA title="Sign Out" icon={false} />
               </div>
+
+              {/* menu icon */}
+              <button
+                className="outline-none ml-1 md:hidden"
+                onClick={() => setOpenMenu((prev) => !prev)}
+              >
+                {openMenu ? (
+                  <FontAwesomeIcon
+                    icon={faXmark}
+                    className="w-10 h-6 text-orange-500 font-bold"
+                  />
+                ) : (
+                  <FontAwesomeIcon
+                    icon={faBarsStaggered}
+                    className="w-10 h-6 text-orange-500 font-bold"
+                  />
+                )}
+              </button>
+
+              {/* mobile menu */}
+              {openMenu && (
+                <div className="absolute top-14 right-0 max-w-56 min-h-60 py-10 px-5 shadow-md rounded-2xl bg-white/90 backdrop-blur-md z-10">
+                  <div className="flex flex-col md:hidden gap-4">
+                    <Link
+                      href={"/dashboard"}
+                      className="text-sm font-medium text-gray-500"
+                      onClick={() => setOpenMenu((prev) => !prev)}
+                    >
+                      Dashboard
+                    </Link>
+                    <Link
+                      href={"/dashboard/profile"}
+                      className="text-sm font-medium text-gray-500"
+                      onClick={() => setOpenMenu((prev) => !prev)}
+                    >
+                      Profile
+                    </Link>
+
+                    {/* logout button */}
+                    <CTA icon={false} title={"Logout"} />
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
