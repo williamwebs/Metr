da0ceaeac24d5c59a4a09fc1297f4538a5551ae2
William <anazawilliam1@gmail.com>
2024-03-20 03:19:23 +0100

    dashboard + form

--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -1,9 +1,10 @@
+import CTA from "@/components/button/CTA";
 import { faHand } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 const Dashboard = () => {
-  const profileCompleted = false;
+  const profileCompleted = true;
 
   return (
     <main>
@@ -16,7 +17,11 @@ const Dashboard = () => {
             Welcome to Your <span className="text-black font-bold">Metr:</span>
           </h1>
           <div>
-            <FontAwesomeIcon icon={faHand} className="w-6 text-orange-500" />
+            <FontAwesomeIcon
+              icon={faHand}
+              className="w-6 text-orange-500"
+              shake
+            />
           </div>
         </div>
       </div>
@@ -35,7 +40,55 @@ const Dashboard = () => {
       )}
 
       {/* display only if the user has completed his profile */}
-      {profileCompleted && <div>Completed Profile!</div>}
+      {profileCompleted && (
+        <div className="mx-auto max-w-md my-10">
+          {/* get electricity biller from paybeta.ng  */}
+          {/* form - meter_number meter_type biller_type amount reference */}
+          <form>
+            <div className="flex flex-col gap-4">
+              <div>
+                <label htmlFor="name">meter_number</label>
+                <input
+                  type="number"
+                  name="meter_number"
+                  placeholder="04273972845"
+                  className="block w-full border rounded p-2 outline-none"
+                />
+                validate meter_number before proceeding
+              </div>
+
+              <div>
+                <label htmlFor="meter_type">meter_type</label>
+                fetch from the api (prepaid / postpaid)
+              </div>
+
+              <div>
+                <label htmlFor="biller_type">biller_type</label>
+                <input
+                  type="text"
+                  name="biller_type"
+                  placeholder="ikeja-electric"
+                  className="block w-full border rounded p-2 outline-amber-500"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="amount">amount</label>
+                <input
+                  type="number"
+                  name="amount"
+                  placeholder="amount"
+                  className="block w-full border rounded p-2 outline-amber-500"
+                />
+              </div>
+            </div>
+
+            <div className="my-10 w-full block mx-auto">
+              <CTA title={"Purchase"} icon={true} />
+            </div>
+          </form>
+        </div>
+      )}
     </main>
   );
 };
--- app/(dashboard)/dashboard/profile/page.jsx
+++ app/(dashboard)/dashboard/profile/page.jsx
@@ -1,11 +1,88 @@
+import CTA from "@/components/button/CTA";
+import Image from "next/image";
 import React from "react";
 
 const Profile = () => {
   return (
-    <div>
+    <div className="mx-auto max-w-3xl">
       {/* display a form that fetches the user details from the db auth */}
       {/* the form should also accept users phone number  */}
-      Profile
+      <div className="flex flex-col md:flex-row items-start gap-10 md:gap-20">
+        <div className="rounded-full flex items-center justify-center shadow-md">
+          <Image
+            src={"/"}
+            alt=""
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+        </div>
+
+        <div className="w-full">
+          <div>
+            <p className="mb-10">
+              Fill in your{" "}
+              <span className="text-orange-500 font-bold">Phone number</span> &{" "}
+              <span className="text-orange-500 font-bold">Meter number</span> so
+              you dont have to type them manually everytime you want to purchase
+              energy. Also, it helps us send you{" "}
+              <span className="text-orange-500 font-bold">
+                SMS notification
+              </span>{" "}
+              when your subscription is getting low.
+            </p>
+          </div>
+          <form>
+            <div className="flex flex-col gap-4">
+              <div>
+                <label htmlFor="name">Full Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="fullname"
+                  className="block w-full border rounded p-2 outline-none"
+                  readOnly
+                />
+              </div>
+
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="block w-full border rounded p-2 outline-none"
+                  readOnly
+                />
+              </div>
+
+              <div>
+                <label htmlFor="phone">Phone Number</label>
+                <input
+                  type="tel"
+                  name="phone"
+                  placeholder="phone"
+                  className="block w-full border rounded p-2 outline-amber-500"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="meter">Meter Number</label>
+                <input
+                  type="number"
+                  name="meter"
+                  placeholder="meter"
+                  className="block w-full border rounded p-2 outline-amber-500"
+                />
+              </div>
+            </div>
+
+            <div className="my-10 w-full block mx-auto">
+              <CTA title={"Update profile"} icon={false} />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
