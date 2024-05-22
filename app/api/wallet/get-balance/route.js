151053c47d66602649caecff4e7ac5116198e1b3
William <anazawilliam1@gmail.com>
2024-05-22 15:51:31 +0100

    comments in api route

--- app/api/wallet/get-balance/route.js
+++ app/api/wallet/get-balance/route.js
@@ -10,9 +10,11 @@ export const GET = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
 
+    // check if the user has a wallet created already attached to their email address
     const userWallet = await Wallet.findOne({ user: session?.user?.email });
     console.log(userWallet);
 
+    // check for the user's balance from the wallet collection in the db
     if (userWallet) {
       const currentBalance = userWallet.balance;
       console.log(currentBalance);
@@ -20,32 +22,23 @@ export const GET = async () => {
       return NextResponse.json({
         balance: currentBalance,
       });
-      // return new Response(
-      //   JSON.stringify({
-      //     balance: currentBalance,
-      //   }),
-      //   {
-      //     status: 200,
-      //   }
-      // );
     }
+
+    // if no wallet found attacked to user email, create a new wallet for the user
     const newUserWallet = new Wallet({
       user: session?.user?.email,
       balance: "0",
     });
 
     await newUserWallet.save();
-
     console.log(newUserWallet);
+
     return NextResponse.json({
       message: "User wallet created successfully",
     });
-
-    // return new Response(JSON.stringify(newUserWallet), { status: 201 });
   } catch (error) {
     return NextResponse.json({
       error: `Failed to register user wallet: ${error.message}`,
     });
-    return new Response("Failed to register user wallet!", { status: 500 });
   }
 };
